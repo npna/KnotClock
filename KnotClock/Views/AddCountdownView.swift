cc52ebd8e4dbf2bc3b5c9605932514eacf1c8158
NA <80475242+npna@users.noreply.github.com>
2023-03-11 20:24:24 +0000

    Fix add sheet not working in Menubar

--- KnotClock/Views/AddCountdownView.swift
+++ KnotClock/Views/AddCountdownView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct AddCountdownView: View {
-    @Environment(\.managedObjectContext) var moc
     @Environment(\.dismiss) private var dismiss
     @State private var selectedTab: String
     @State private var title = ""
@@ -18,6 +17,7 @@ struct AddCountdownView: View {
     @State private var alertIsPresented = false
     @State private var alertMessage = ""
     
+    private let moc = DataController.context
     private var isInMenubar: Bool
     
     init(isInMenubar: Bool = false) {
