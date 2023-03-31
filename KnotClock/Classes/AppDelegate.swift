b1954f647c227c4a147a502a32c2be2329088981
NA <80475242+npna@users.noreply.github.com>
2023-03-31 21:47:46 +0000

    Hide main window on launch if application is set to run in menubar only (macOS)

--- KnotClock/Classes/AppDelegate.swift
+++ KnotClock/Classes/AppDelegate.swift
@@ -12,5 +12,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return false
     }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let showDock = UserDefaults.standard.object(forKey: K.StorageKeys.userPreferences) as? String,
+           let decoded = try? JSONDecoder().decode(DefaultUserPreferences.self, from: Data(showDock.utf8)),
+           let window = NSApp.windows.first,
+           decoded.showDockIcon == false
+        {
+            window.close()
+        }
+    }
 }
 #endif
